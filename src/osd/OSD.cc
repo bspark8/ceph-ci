3082eda71bd6f0011a91048ba0c258a256aed5ff
Byungsu Park <bspark8@sk.com>
2017-12-05 11:42:56 +0900

    osd: add mClockPoolQueue to support pool unit qos.
    Signed-off-by: Byungsu Park <bspark8@sk.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -9967,6 +9967,9 @@ std::ostream& operator<<(std::ostream& out, const OSD::io_queue& q) {
   case OSD::io_queue::mclock_client:
     out << "mclock_client";
     break;
+  case OSD::io_queue::mclock_pool:
+    out << "mclock_pool";
+    break;
   }
   return out;
 }
